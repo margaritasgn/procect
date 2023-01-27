d51df777d3536e4e9a359c5de4901d0369544f95
Margarita <example@gmail.com>
2023-01-27 13:30:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,113 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Margarita';
+
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a'); 
+
+// let number = 4.6;
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = 'Alex';
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpn', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]); 
+
+// alert('Hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+
+// const answer = +prompt("Are you 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Margarita';
+// alert(`Hello, ${user}`);
+
+
+
+
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+let incr = 10,
+    degr = 10;
+
+// ++incr;
+// --degr;
+
+console.log(++incr);
+console.log(--degr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 === 8);
+
+console.log(2 + 2 * 2 !== 8);
+
+console.log(2 + 2 * 2 != '6'); // false 
+console.log(2 + 2 * 2 !== '6'); // true потому что 6 это число , а не строка
+
